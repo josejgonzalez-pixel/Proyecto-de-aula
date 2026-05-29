c9abb4f4ffb0c023273d98d801e1bc2555e5ad2d
adruidiaz <adruidiaz@unicesar.edu.co>
2026-05-29 18:58:01 -0500

    feat: clase TransaccionService y toda su logica

--- src/Service/TransaccionService.java
+++ src/Service/TransaccionService.java
@@ -4,10 +4,61 @@
  */
 package Service;
 
+import DAO.Response;
+import DAO.TransaccionDao;
+import Model.Transaccion;
 /**
  *
  * @author hp
  */
 public class TransaccionService {
-    
+
+    private TransaccionDao repo;
+
+    public TransaccionService() {
+        repo = new TransaccionDao();
+    }
+
+    // INSERTAR
+    public Response<Transaccion> insertar(Transaccion t) {
+        return repo.insertar(t);
+    }
+
+    // ACTUALIZAR
+    public Response<Transaccion> actualizar(Transaccion t) {
+        return repo.actualizar(t);
+    }
+
+    // ELIMINAR
+    public Response<Transaccion> eliminar(int id) {
+        return repo.eliminar(id);
+    }
+
+    // BUSCAR POR ID
+    public Response<Transaccion> obtenerPorId(int id) {
+        return repo.obtenerPorId(id);
+    }
+
+    // LISTAR TODAS
+    public Response<Transaccion> obtenerTodos() {
+        return repo.obtenerTodos();
+    }
+
+    // OBTENER BALANCE TOTAL
+    public double obtenerBalanceTotal() {
+
+        double total = 0;
+
+        Response<Transaccion> respuesta = repo.obtenerTodos();
+
+        if (respuesta.getLista() != null) {
+
+            for (Transaccion t : respuesta.getLista()) {
+
+                total += t.getMonto();
+            }
+        }
+
+        return total;
+    }
 }
