38a5eb54e5c73eb44ac29b9cdc40aeabe1e4ff06
adruidiaz <adruidiaz@unicesar.edu.co>
2026-05-30 00:40:09 -0500

    feat: clase CategoriaService con sus funcionalidades y logica

--- src/Service/CategoriaService.java
+++ src/Service/CategoriaService.java
@@ -4,10 +4,52 @@
  */
 package Service;
 
+import DAO.CategoriaDao;
+import DAO.Response;
+import Model.Categoria;
 /**
  *
  * @author hp
  */
 public class CategoriaService {
-    
+
+    private CategoriaDao repo;
+
+    public CategoriaService() {
+        repo = new CategoriaDao();
+    }
+
+    // INSERTAR
+    public Response<Categoria> insertar(Categoria categoria) {
+        return repo.insertar(categoria);
+    }
+
+    // ACTUALIZAR
+    public Response<Categoria> actualizar(Categoria categoria) {
+        return repo.actualizar(categoria);
+    }
+
+    // ELIMINAR
+    public Response<Categoria> eliminar(int id) {
+        return repo.eliminar(id);
+    }
+
+    // OBTENER POR ID
+    public Response<Categoria> obtenerPorId(int id) {
+        return repo.obtenerPorId(id);
+    }
+
+    // OBTENER TODAS
+    public Response<Categoria> obtenerTodos() {
+        return repo.obtenerTodos();
+    }
+
+    // VERIFICAR SI LA CATEGORÍA ESTÁ EN USO
+    public boolean categoriaEnUso(int idCategoria) {
+
+        Response<Categoria> respuesta = repo.obtenerPorId(idCategoria);
+
+        return respuesta.isSuccess()
+                && respuesta.getObjeto() != null;
+    }
 }
