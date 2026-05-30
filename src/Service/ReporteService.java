e653e5f25e4bdcbf4c1be405466527042d7f32f4
adruidiaz <adruidiaz@unicesar.edu.co>
2026-05-29 19:08:21 -0500

    feat: clase ReporteService con sus funcionalidades y su logica

--- src/Service/ReporteService.java
+++ src/Service/ReporteService.java
@@ -4,10 +4,80 @@
  */
 package Service;
 
+import DAO.ReporteDao;
+import DAO.TransaccionDao;
+import DAO.Response;
+import Model.Reporte;
+import Model.Transaccion;
 /**
  *
  * @author hp
  */
 public class ReporteService {
-    
+
+    private ReporteDao reporteDao;
+    private TransaccionDao transaccionDao;
+
+    public ReporteService() {
+        reporteDao = new ReporteDao();
+        transaccionDao = new TransaccionDao();
+    }
+
+    // INSERTAR
+    public Response<Reporte> insertar(Reporte reporte) {
+        return reporteDao.insertar(reporte);
+    }
+
+    // ACTUALIZAR
+    public Response<Reporte> actualizar(Reporte reporte) {
+        return reporteDao.actualizar(reporte);
+    }
+
+    // ELIMINAR
+    public Response<Reporte> eliminar(int id) {
+        return reporteDao.eliminar(id);
+    }
+
+    // BUSCAR POR ID
+    public Response<Reporte> obtenerPorId(int id) {
+        return reporteDao.obtenerPorId(id);
+    }
+
+    // LISTAR TODOS
+    public Response<Reporte> obtenerTodos() {
+        return reporteDao.obtenerTodos();
+    }
+
+    /**
+     * Obtiene las transacciones para generar graficos.
+     * Puedes filtrar posteriormente por ingresos o gastos.
+     */
+    public Response<Transaccion> obtenerDatosParaGrafico(String tipo) {
+
+        Response<Transaccion> respuesta =
+                transaccionDao.obtenerTodos();
+
+        return respuesta;
+    }
+
+    /**
+     * Calcula el balance financiero total.
+     */
+    public double obtenerResumenFinanciero() {
+
+        double total = 0;
+
+        Response<Transaccion> respuesta =
+                transaccionDao.obtenerTodos();
+
+        if (respuesta.getLista() != null) {
+
+            for (Transaccion t : respuesta.getLista()) {
+
+                total += t.getMonto();
+            }
+        }
+
+        return total;
+    }
 }
