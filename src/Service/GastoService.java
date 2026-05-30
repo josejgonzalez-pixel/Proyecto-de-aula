4ca52513a9fdc2c60ffd22c27f3114e37032d577
adruidiaz <adruidiaz@unicesar.edu.co>
2026-05-30 00:33:06 -0500

    feat: clase GastoService con sus funcionalidades y logica

--- src/Service/GastoService.java
+++ src/Service/GastoService.java
@@ -3,11 +3,61 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Service;
+import DAO.GastoDao;
+import DAO.Response;
+import Model.Gasto;
 
 /**
  *
  * @author hp
  */
 public class GastoService {
-    
+
+    private GastoDao repo;
+
+    public GastoService() {
+        repo = new GastoDao();
+    }
+
+    // INSERTAR
+    public Response<Gasto> insertar(Gasto gasto) {
+        return repo.insertar(gasto);
+    }
+
+    // ACTUALIZAR
+    public Response<Gasto> actualizar(Gasto gasto) {
+        return repo.actualizar(gasto);
+    }
+
+    // ELIMINAR
+    public Response<Gasto> eliminar(int id) {
+        return repo.eliminar(id);
+    }
+
+    // OBTENER POR ID
+    public Response<Gasto> obtenerPorId(int id) {
+        return repo.obtenerPorId(id);
+    }
+
+    // OBTENER TODOS
+    public Response<Gasto> obtenerTodos() {
+        return repo.obtenerTodos();
+    }
+
+    // CALCULAR TOTAL DE GASTOS
+    public double calcularTotalGastos() {
+
+        double total = 0;
+
+        Response<Gasto> respuesta = repo.obtenerTodos();
+
+        if (respuesta.getLista() != null) {
+
+            for (Gasto gasto : respuesta.getLista()) {
+                total += gasto.getMonto();
+            }
+        }
+
+        return total;
+    }
 }
